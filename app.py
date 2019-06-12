8f6ddab004b876d58298a11473bfb64348811015
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-06-12 15:10:23 -0400

    Can now receive files via upload-file route'

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+import sqlite3
+
 
 app = Flask(__name__)
 
@@ -7,11 +10,13 @@ app = Flask(__name__)
 ## A database entry is then made that contains at the minimum (FileName, TimeOfUpload, FiveMostCommonWords)
 @app.route('/upload-file', methods=['GET', 'POST'])
 def process_file():
-	if(request.method == 'GET'):
-    	return 'Hello, World!'
-    else:
-    	# Do processing here
-    	return 'Hello, World!'
+	if request.method == 'GET':
+		return 'Hello, World!\n'
+	else:
+	# Do processing here
+		pdf = request.files['pdf_file']
+		print('PDF file received with filename ' + pdf.filename)
+		return 'Hello, World!\n'
 
 
 
@@ -19,9 +24,8 @@ def process_file():
 ## Returns at the very least, the name of file, time of upload and the 5 most common words for 
 ## all previously uploaded PDF's
 @app.route('/get-common-words')
-def process_file():
-	if(request.method == 'GET'):
-    	return 'Hello, World!'
-    else:
-    	# Do processing here
-    	return 'Hello, World!'
+def get_common_words():
+	if request.method == 'GET':
+		return 'Hello, World!\n'
+   	else: 
+		return 'Hello, World!\n'
