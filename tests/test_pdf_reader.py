f7a8a046cfb3f5f89c2654ae710d93b2a63d5e23
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-06-13 17:00:13 -0400

    Added boilterplate unit testing code

---
+++ tests/test_pdf_reader.py
@@ -0,0 +1,18 @@
+import pytest
+from flaskpdfreader.db import get_db
+
+
+def test_sanitize_stats():
+    pass
+
+
+def test_make_json():
+    pass
+
+
+def test_process_file():
+    pass
+
+
+def test_get_common_words():
+    pass
