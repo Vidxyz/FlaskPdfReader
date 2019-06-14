6662b65b2bb9247e9e2ef8b4ea5ef07e479982ec
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-06-13 20:10:29 -0400

    Added more test coverage

--- tests/test_pdf_reader.py
+++ tests/test_pdf_reader.py
@@ -13,6 +13,34 @@ FIRST_TESTING_STATS = [(1, 'sample1.pdf', datetime.datetime(2019, 6, 13, 20, 13,
                         (1, 'sample1.pdf', datetime.datetime(2019, 6, 13, 20, 13, 13), 1, 'burgers'), ]
 CLEANED_FIRST_STATS = [['sample1.pdf', datetime.datetime(2019, 6, 13, 20, 13, 13), 
                         [('banana', 9), ('potato',7), ('onion',4), ('hotdogs',2), ('burgers',1)]]]
+JSON_FIRST_STATS = [
+                    {'filename':'sample1.pdf',
+                    'timestamp': datetime.datetime(2019, 6, 13, 20, 13, 13),
+                    'most_common_words': [
+                            {
+                                'count': 9,
+                                'word': 'banana'
+                            },
+                            {
+                                'count': 7,
+                                'word': 'potato'
+                            },
+                            {
+                                'count': 4,
+                                'word': 'onion'
+                            },
+                            {
+                                'count': 2,
+                                'word': 'hotdogs'
+                            },
+                            {
+                                'count': 1,
+                                'word': 'burgers'
+                            },
+                        ]
+                    }
+                    ]
+
 
 
 SECOND_TESTING_STATS = [(1, 'sample1.pdf', datetime.datetime(2019, 6, 13, 20, 13, 13), 9, 'banana'), 
@@ -25,13 +53,70 @@ SECOND_TESTING_STATS = [(1, 'sample1.pdf', datetime.datetime(2019, 6, 13, 20, 13
                         (2, 'sample2.pdf', datetime.datetime(2019, 6, 11, 20, 13, 13), 11, 'is'), 
                         (2, 'sample2.pdf', datetime.datetime(2019, 6, 11, 20, 13, 13), 9, 'just'), 
                         (2, 'sample2.pdf', datetime.datetime(2019, 6, 11, 20, 13, 13), 4, 'another'), 
-                        (2, 'sample2pdf', datetime.datetime(2019, 6, 11, 20, 13, 13), 3, 'test'),
+                        (2, 'sample2.pdf', datetime.datetime(2019, 6, 11, 20, 13, 13), 3, 'test'),
                         ]
 CLEANED_SECOND_STATS = [['sample1.pdf', datetime.datetime(2019, 6, 13, 20, 13, 13), 
                         [('banana', 9), ('potato',7), ('onion',4), ('hotdogs',2), ('burgers',1)]],
                         ['sample2.pdf', datetime.datetime(2019, 6, 11, 20, 13, 13), 
                         [('this', 13), ('is',11), ('just',9), ('another',4), ('test',3)]]
                         ]
+JSON_SECOND_STATS = [
+                        {
+                            'filename':'sample1.pdf',
+                            'timestamp': datetime.datetime(2019, 6, 13, 20, 13, 13),
+                            'most_common_words': [
+                                    {
+                                        'count': 9,
+                                        'word': 'banana'
+                                    },
+                                    {
+                                        'count': 7,
+                                        'word': 'potato'
+                                    },
+                                    {
+                                        'count': 4,
+                                        'word': 'onion'
+                                    },
+                                    {
+                                        'count': 2,
+                                        'word': 'hotdogs'
+                                    },
+                                    {
+                                        'count': 1,
+                                        'word': 'burgers'
+                                    },
+                                ]
+                        }, 
+                        {
+                            'filename':'sample2.pdf',
+                            'timestamp': datetime.datetime(2019, 6, 11, 20, 13, 13),
+                            'most_common_words': [
+                                    {
+                                        'count': 13,
+                                        'word': 'this'
+                                    },
+                                    {
+                                        'count': 11,
+                                        'word': 'is'
+                                    },
+                                    {
+                                        'count': 9,
+                                        'word': 'just'
+                                    },
+                                    {
+                                        'count': 4,
+                                        'word': 'another'
+                                    },
+                                    {
+                                        'count': 3,
+                                        'word': 'test'
+                                    },
+                                ]
+                        }, 
+                    ]
+
+
+
 
 
 def test_sanitize_stats():
@@ -43,13 +128,23 @@ def test_sanitize_stats():
     cleaned_stats = sanitize_stats(stats)
     assert cleaned_stats == CLEANED_FIRST_STATS
 
-    stats = EMPTY_TESTING_STATS
+    stats = SECOND_TESTING_STATS
     cleaned_stats = sanitize_stats(stats)
-    assert cleaned_stats == []
+    assert cleaned_stats == CLEANED_SECOND_STATS
 
 
 def test_make_json():
-    pass
+    stats = EMPTY_TESTING_STATS
+    json_stats = make_json(stats)
+    assert json_stats == []
+
+    stats = CLEANED_FIRST_STATS
+    json_stats = make_json(stats)
+    assert json_stats == JSON_FIRST_STATS
+
+    stats = CLEANED_SECOND_STATS
+    json_stats = make_json(stats)
+    assert json_stats == JSON_SECOND_STATS
 
 
 def test_process_file():
